4ce0078c55b1374e61a914095c8c20268209c9ce
GamerLucas2 <lucasonckent@hotmail.com>
2026-05-27 12:57:56 -0300

    Input test

---
+++ ProjetoEscritorioRuim/Assets/Scripts/InputTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            Debug.Log("A key was pressed");
+        }
+    }
+}
