7171720f98392f0817ee9eb8125e4e926d5a8feb
GamerLucas2 <lucasonckent@hotmail.com>
2026-06-15 20:43:58 -0300

    Item Pick up
    - Player picks up items and puts them back where he is looking

--- ProjetoEscritorioRuim/Assets/Scripts/PlayerInteract.cs
+++ ProjetoEscritorioRuim/Assets/Scripts/PlayerInteract.cs
@@ -1,20 +1,33 @@
 using System;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerInteract : MonoBehaviour
 {
-    LayerMask interactMask;
-
     private InputSystem_Actions inputSystem;
-    private InputAction interact;
+    InputAction interact;
+
+    [SerializeField] LayerMask interactMask;
+    [SerializeField] float maxDistance;
+    
+    bool isInteracting = false;
+    [SerializeField] private GameObject itemIndicator;
+    [SerializeField] private GameObject interactObject;
+    bool objectIsPresent = false;
+    
     
     void Awake()
     {
         inputSystem = new InputSystem_Actions();
         interact = inputSystem.Player.Interact;
-        
-        interactMask = LayerMask.GetMask("Interact");
+    }
+
+    private void Start()
+    {
+        itemIndicator.SetActive(false);
+        Ray cameraRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
     }
 
     private void OnEnable()
@@ -33,17 +46,52 @@ public class PlayerInteract : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (interact.WasPressedThisFrame())
+        if (interact.WasPressedThisFrame() && !isInteracting)
+        {
+            isInteracting = true;
             InteractWithItem();
+            if (interactObject.CompareTag("Item"))
+            {
+                itemIndicator.SetActive(true);
+                interactObject.transform.position = new Vector3(0, 100000, 0);
+            }
+        }
+        else if (interact.WasPressedThisFrame() && isInteracting)
+        {
+            PlaceObjBack();
+            if (objectIsPresent)
+            {
+                isInteracting = false;
+                itemIndicator.SetActive(false);
+                
+            }
+        }
     }
 
     private void InteractWithItem()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, interactMask))
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, interactMask))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
             Debug.Log("Hit:  " + hit.transform.gameObject.name);
+
+            interactObject = hit.transform.gameObject;
         }
+    }
+
+    void PlaceObjBack()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxDistance, interactMask))
+            if (hit.transform.CompareTag("Place"))
+            {
+                interactObject.transform.position = hit.point;
+                interactObject = null;
+                objectIsPresent = true;
+            }
+            else
+                objectIsPresent = false;
     }
 }
