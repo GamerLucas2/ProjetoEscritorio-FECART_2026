a0d0021de49eecf11993721b72beda9764991867
GamerLucas2 <lucasonckent@hotmail.com>
2026-06-17 13:01:36 -0300

    Ajustments 2

--- ProjetoEscritorioRuim/Assets/Scripts/TaskSystem.cs
+++ ProjetoEscritorioRuim/Assets/Scripts/TaskSystem.cs
@@ -1,42 +1,31 @@
-using System;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TaskSystem : MonoBehaviour
 {
-    //InputSystem_Actions inputSystemActions;
-    //InputAction interaction;
     public float completedTasks=0f;
     private bool holding = false;
     [SerializeField] private string tasksLeft;
     [SerializeField] private TextMeshProUGUI taskNumber;
-/*private void Awake()
-{
-    inputSystemActions = new InputSystem_Actions();
-    interaction = inputSystemActions.Player.Interact;
-    interaction.Enable();
-}*/
-private void Update()
-{
-    taskNumber.text= "Tasks Done: "+completedTasks + tasksLeft;
-}
-private void OnTriggerEnter(Collider taskCollider)
-{
-    if (taskCollider.CompareTag("TaskStart") && !holding)
+    
+    private void Update()
     {
-        print("TaskStart");
-        holding = true;
-        Destroy(taskCollider.gameObject);
+        taskNumber.text= "Tasks Done: "+completedTasks + tasksLeft;
     }
-    else if (taskCollider.CompareTag("TaskEnd") && holding)
+    private void OnTriggerEnter(Collider taskCollider)
     {
-        print("TaskEnd");
-        holding = false;
-        taskCollider.gameObject.tag = "TaskComplete";
-        completedTasks++;
+        if (taskCollider.CompareTag("TaskStart") && !holding)
+        {
+            print("TaskStart");
+            holding = true;
+            Destroy(taskCollider.gameObject);
+        }
+        else if (taskCollider.CompareTag("TaskEnd") && holding)
+        {
+            print("TaskEnd");
+            holding = false;
+            taskCollider.gameObject.tag = "TaskComplete";
+            completedTasks++;
+        }
     }
-}
 }
